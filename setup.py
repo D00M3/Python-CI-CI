7fe1bd8eb1b128c9c5ecd4267e6f9377407344a4
D00M3 <97620051+D00M3@users.noreply.github.com>
2022-04-19 23:46:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="timechecker",
+    name="bob321",
     version="0.0.1",
-    author="bob321",
+    author="Zaki",
     author_email="Apollo12123451@gmail.com",
     url="https://bit.ly/edeediong-resume",
     description="An application that informs you of the time in different locations and timezones",
